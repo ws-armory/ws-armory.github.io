c6f7a9c06029c01d36c1304fef1503919cf19a5a
Logan Ogden <loganogden@gmail.com>
2014-12-16 10:56:30 -0600

    Updated styling

--- js/armory.js
+++ js/armory.js
@@ -137,7 +137,7 @@ function loadItems(obj){
 	    error = document.getElementById('error');
 
 	if (obj == null || Object.getOwnPropertyNames(obj).length === 0) {
-		error.innerHTML = "No items given!";
+		error.innerHTML = "No items given.</br></br>Please use the Wildstar Armory addon to generate item lists.";
 		return;
 	}
 
